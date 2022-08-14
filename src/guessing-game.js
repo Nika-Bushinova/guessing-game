eb5362122ebb8d641fc5ed2b9d9f867aea5e9ad1
Nika-Bushinova <n190696@yandex.ru>
2022-08-14 08:07:27 -0400

    add functions

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
     guess() {
-
+        if (this.max < 1) { return 0 }
+        this.middle = Math.round((this.max + this.min) / 2)
+        return this.middle
+        //алгоритм бинарного поиска
+        //возвратить ответ-предположение в каждой попытке
     }
 
     lower() {
-
+        return this.max = this.middle
+        //переопределить верхнюю границу
     }
 
     greater() {
-
+        return this.min = this.middle
+        //переопределить нижнюю границу
     }
 }
 
